234ea9f27d4a10c10d7b6a2d76a12961d20506f9
UltraDeveloper7 <75303541+UltraDeveloper7@users.noreply.github.com>
2024-01-18 01:33:45 +0200

    Update MapView.py

--- MapView.py
+++ MapView.py
@@ -1,42 +1,46 @@
 import customtkinter as ctk
+import os
 import sys
 import subprocess
 
-subprocess.check_call([sys.executable, "-m", "pip", "install", "tkintermapview"])
 from tkintermapview import TkinterMapView
 
 ctk.set_default_color_theme("blue")
 
 
-class App(ctk.CTk):
+class App:
 
     APP_NAME = "Map-View/Phone Locator"
     WIDTH = 800
     HEIGHT = 500
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, master=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.master = master
+        self.top = ctk.CTkToplevel(self.master)
+        # Bring the window to the top
+        self.top.lift()
+        self.top.grab_set()
+        self.top.title(App.APP_NAME)
+        self.top.geometry(str(App.WIDTH) + "x" + str(App.HEIGHT))
+        self.top.minsize(App.WIDTH, App.HEIGHT)
 
-        self.title(App.APP_NAME)
-        self.geometry(str(App.WIDTH) + "x" + str(App.HEIGHT))
-        self.minsize(App.WIDTH, App.HEIGHT)
-
-        self.protocol("WM_DELETE_WINDOW", self.on_closing)
-        self.bind("<Command-q>", self.on_closing) # type: ignore
-        self.createcommand('tk::mac::Quit', self.on_closing)
+        self.top.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.top.bind("<Command-q>", self.on_closing) # type: ignore
+        #self.top.createcommand('tk::mac::Quit', self.on_closing)
 
         self.marker_list = []
 
         # ============ create two CTkFrames ============
 
-        self.grid_columnconfigure(0, weight=0)
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.top.grid_columnconfigure(0, weight=0)
+        self.top.grid_columnconfigure(1, weight=1)
+        self.top.grid_rowconfigure(0, weight=1)
 
-        self.frame_left = ctk.CTkFrame(master=self, width=150, corner_radius=0, fg_color=None)
+        self.frame_left = ctk.CTkFrame(master=self.top, width=150, corner_radius=0, fg_color=None)
         self.frame_left.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
 
-        self.frame_right = ctk.CTkFrame(master=self, corner_radius=0)
+        self.frame_right = ctk.CTkFrame(master=self.top, corner_radius=0)
         self.frame_right.grid(row=0, column=1, rowspan=1, pady=0, padx=0, sticky="nsew")
 
         # ============ frame_left ============
@@ -73,6 +77,7 @@ class App(ctk.CTk):
         self.frame_right.grid_columnconfigure(1, weight=0)
         self.frame_right.grid_columnconfigure(2, weight=1)
 
+
         self.map_widget = TkinterMapView(self.frame_right, corner_radius=0)
         self.map_widget.grid(row=1, rowspan=1, column=0, columnspan=3, sticky="nswe", padx=(0, 0), pady=(0, 0))
         self.map_widget.bind("<Button-1>",self.map_widget.mouse_click)
@@ -104,6 +109,8 @@ class App(ctk.CTk):
         print("Add marker:", coords)
         new_marker = self.map_widget.set_marker(coords[0], coords[1])
         self.marker_list.append(new_marker)
+        # Center the map on the new marker
+        self.map_widget.set_position(coords[0], coords[1])
         
     def left_click_event(self,coordinates_tuple):
         clicked_position = coordinates_tuple
@@ -135,15 +142,11 @@ class App(ctk.CTk):
             self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22) 
 
     def on_closing(self, event=0):
-        self.destroy()
+        self.top.destroy()
 
     def start(self, coordinates=None):
         if coordinates is not None:
             self.add_marker_event(coordinates)
-        self.mainloop()
+        self.top.mainloop()
 
-
-if __name__ == "__main__":
-    app = App()
-    app.start()
     
